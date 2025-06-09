0d20f90778eb481d8d8ccf1c16a8bf51b0ed8dc9
Cenzino21 <vincenzolabella2018@gmail.com>
2025-06-09 13:04:58 +0200

    Commit 3, implementazione controller

--- Simulatore_Tasse_Uni_Backend/src/main/java/com/example/Simulatore_Tasse_Uni_Backend/SimulatoreTasseUniBackendApplication.java
+++ Simulatore_Tasse_Uni_Backend/src/main/java/com/example/Simulatore_Tasse_Uni_Backend/SimulatoreTasseUniBackendApplication.java
@@ -1,6 +1,6 @@
 package com.example.Simulatore_Tasse_Uni_Backend;
 
-import com.example.Simulatore_Tasse_Uni_Backend.calcolotassa.CalcoloTassa;
+import com.example.Simulatore_Tasse_Uni_Backend.models.CalcoloTassa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,9 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimulatoreTasseUniBackendApplication {
 
 	public static void main(String[] args) {
-		CalcoloTassa calcoloTassa = new CalcoloTassa();
-		calcoloTassa.calcoloTasse();
-
 		SpringApplication.run(SimulatoreTasseUniBackendApplication.class, args);
 	}
 
---
+++ Simulatore_Tasse_Uni_Backend/src/main/java/com/example/Simulatore_Tasse_Uni_Backend/models/CalcoloTassa.java
@@ -0,0 +1,32 @@
+package com.example.Simulatore_Tasse_Uni_Backend.models;
+import java.util.Scanner;
+
+public class CalcoloTassa {
+    private float valoreISEE; //Valore ISEE espresso in decimale
+
+    public String calcolaFascia(float valoreISEE) {
+        if(valoreISEE < 1000.00) {
+            return "No tax area";
+        } else if (valoreISEE >= 1000.00 && valoreISEE < 2000.00) {
+            return "Fascia 1, la tua tassa è 100 euro";
+        } else if (valoreISEE >= 2000.00 && valoreISEE < 3000.00) {
+            return "Fascia 2, la tua tassa è 200 euro";
+        } else if (valoreISEE >= 3000.00 && valoreISEE < 4000.00) {
+            return "Fascia 3, la tua tassa è 300 euro";
+        } else {
+            return "Fascia 4, la tua tassa è 1000 euro";
+        }
+    }
+
+    public float getValoreISEE() {
+        return valoreISEE;
+    }
+
+    public void setValoreISEE(float valoreISEE) {
+        this.valoreISEE = valoreISEE;
+    }
+
+
+
+
+}
